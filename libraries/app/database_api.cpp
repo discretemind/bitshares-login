4c82273ae98d54283dc1d6f60be5ee18f2f94193
Denis Korkhov <denis.korkhov@discretemind.com>
2019-03-25 17:10:00 +0300

    Book subscribe

--- libraries/app/database_api.cpp
+++ libraries/app/database_api.cpp
@@ -2577,12 +2577,18 @@ void database_api_impl::on_pending_orders(const signed_transaction& trx, uint32_
                     case operation::tag<limit_order_create_operation>::value:
                         new_order = op.op.get<limit_order_create_operation>();
                         std::cout << "new_order: " << typeid(*new_order).name() << '\n';
+
+                        const auto& value = fc::variant(new_order,2)
+
+                        std::cout << "order str: " << value.as_string() << '\n';
+                        const auto& value = fc::variant(*new_order,1)
+                        std::cout << "order str2: " << value.as_string() << '\n';
     ////                    limit_order ord;
     ////                    ord.seller = (*new_order).seller;
     ////                    ord.base = (*new_order).amount_to_sell;
     ////                    ord.quote = (*new_order).min_to_receive;
 //                        orders.push_back(*new_order);
-                        _new_orders_callback(fc::variant(new_order,2));
+                        _new_orders_callback(fc::variant(*new_order,2));
 //                        _new_orders_callback(fc::variant(orders,2));
                         break;
             }
