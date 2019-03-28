63d8d9975bae4add78421ee655916379231cfc3f
Denis Korkhov <denis.korkhov@discretemind.com>
2019-03-28 12:02:07 +0300

    fetch applyed order bookPublish datafast trx handling

--- libraries/chain/db_block.cpp
+++ libraries/chain/db_block.cpp
@@ -867,13 +867,13 @@ namespace graphene {
 
         vector<optional < asset_object>>
         database::lookup_asset_symbols(const vector<string> &symbols_or_ids) const {
-            const auto &assets_by_symbol = _db.get_index_type<asset_index>().indices().get<by_symbol>();
+            const auto &assets_by_symbol = get_index_type<asset_index>().indices().get<by_symbol>();
             vector<optional < asset_object> > result;
             result.reserve(symbols_or_ids.size());
             std::transform(symbols_or_ids.begin(), symbols_or_ids.end(), std::back_inserter(result),
                            [this, &assets_by_symbol](const string &symbol_or_id) -> optional <asset_object> {
                                if (!symbol_or_id.empty() && std::isdigit(symbol_or_id[0])) {
-                                   auto ptr = _db.find(variant(symbol_or_id, 1).as<asset_id_type>(1));
+                                   auto ptr = find(variant(symbol_or_id, 1).as<asset_id_type>(1));
                                    return ptr == nullptr ? optional<asset_object>() : *ptr;
                                }
                                auto itr = assets_by_symbol.find(symbol_or_id);
