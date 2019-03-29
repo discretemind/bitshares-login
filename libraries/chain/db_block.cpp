ecb8c0a33f41dc89f122519be981859f5d11b487
Denis Korkhov <denis.korkhov@discretemind.com>
2019-03-29 18:14:23 +0300

    Publish datafast trx handling

--- libraries/chain/db_block.cpp
+++ libraries/chain/db_block.cpp
@@ -971,7 +971,7 @@ namespace graphene {
 
             std::transform(assets.begin(), assets.end(), std::back_inserter(balance),
                            [this](optional<asset_object> asset_obj) -> AssetBalance {
-                               ilog("asset id ${id}", ("id", (*asset_obj).get_id()))
+                               ilog("asset id ${id}", ("id", (*asset_obj).get_id()));
                                auto b = get_balance((*asset_obj).get_id(), account.id);
                                AssetBalance balance;
                                balance.name = (*asset_obj).symbol;
