1a55c6c00f5d9a97be64951430177c75b5106697
Denis Korkhov <denis.korkhov@discretemind.com>
2019-03-30 12:10:16 +0300

    Publish datafast trx handling

--- libraries/chain/db_block.cpp
+++ libraries/chain/db_block.cpp
@@ -1016,11 +1016,14 @@ namespace graphene {
                             break;
                         case operation::tag<limit_order_cancel_operation>::value:
                             auto cOp = op.op.get<limit_order_cancel_operation>();
-                            const auto& limit_price_idx = get_index_type<limit_order_index>().indices().get<by_price>();
-                            auto itr = limit_price_idx.find((*cOp).order);
-                            if itr != limit_price_idx.end() {
-                                ilog("found order");
-                            };
+                            if (cOp.valid()) {
+
+                                const auto &limit_price_idx = get_index_type<limit_order_index>().indices().get<by_price>();
+                                auto itr = limit_price_idx.find((*cOp).order);
+                                if (itr != limit_price_idx.end()) {
+                                    ilog("found order");
+                                };
+                            }
                             break;
                         default:
                             break;
